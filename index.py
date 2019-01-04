8a7c493358e1dfb41dd3d68a508685001b1191c0
Kaufmann <kaufmann@kaufmannde-mbp.lan>
2019-01-04 14:34:46 +0800

    update timezone

--- index.py
+++ index.py
@@ -1,11 +1,13 @@
 import json
 import datetime
+import pytz
 
 
 def handler(event, context):
     data = {
         'output': 'Hello World,Owlting is coming,add cloudtrail !!!',
-        'timestamp': datetime.datetime.utcnow().isoformat()
+        'timestamp': datetime.datetime.utcnow().replace(tzinfo=pytz.utc)(),
+'new timestamp':datetime.now(pytz.utc)
     }
     return {'statusCode': 200,
             'body': json.dumps(data),
