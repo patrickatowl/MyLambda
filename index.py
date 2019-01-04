771005ac99e4ff58046aa8b7583815c99164fe8e
Kaufmann <kaufmann@kaufmannde-mbp.lan>
2019-01-04 11:50:27 +0800

    my test

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ import datetime
 
 def handler(event, context):
     data = {
-        'output': 'Hello World',
+        'output': 'Hello World,Owlting is coming',
         'timestamp': datetime.datetime.utcnow().isoformat()
     }
     return {'statusCode': 200,
